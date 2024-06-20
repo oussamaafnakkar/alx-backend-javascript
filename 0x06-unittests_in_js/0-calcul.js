eb564ebf9c281630cc0d2990d22528618a51fb3a
Oussama Afnakkar <124894860+oussamaafnakkar@users.noreply.github.com>
2024-06-20 12:06:05 +0000

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
-
-module.exports = calculateNumber;
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-function getPaymentTokenFromAPI(success) {
-  if (success) {
-    return Promise.resolve({ data: 'Successful response from the API' });
-  }
-}
-
-module.exports = getPaymentTokenFromAPI;
